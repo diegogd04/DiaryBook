cb99b69563f294b97c0758372ff7fca48467e25f
Diego González Díaz <diego2612004@gmail.com>
2025-03-11 15:55:05 +0100

    Crear la barra de navegación y configurar la navegación.

--- app/src/main/java/edu/iesam/diarybook/MainActivity.kt
+++ app/src/main/java/edu/iesam/diarybook/MainActivity.kt
@@ -2,10 +2,24 @@ package edu.iesam.diarybook
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpBottomNavigationBar()
+    }
+
+    private fun setUpBottomNavigationBar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+
+        bottomNavView.setupWithNavController(navController)
     }
 }
