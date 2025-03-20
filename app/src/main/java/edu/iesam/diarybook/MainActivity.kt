934606dde3e575b866278de7e6bb6c21b5ab1c62
Diego González Díaz <diego2612004@gmail.com>
2025-03-20 13:03:17 +0100

    Implementar splash screen.

--- app/src/main/java/edu/iesam/diarybook/MainActivity.kt
+++ app/src/main/java/edu/iesam/diarybook/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.iesam.diarybook
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -9,6 +10,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setUpBottomNavigationBar()
