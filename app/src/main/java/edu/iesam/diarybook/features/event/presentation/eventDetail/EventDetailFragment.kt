8a20825aa2a5c14194afe955bb650c4a90db0fa0
Diego González Díaz <diego2612004@gmail.com>
2025-06-03 18:01:23 +0200

    Modificar toolbar eventos.

--- app/src/main/java/edu/iesam/diarybook/features/event/presentation/eventDetail/EventDetailFragment.kt
+++ app/src/main/java/edu/iesam/diarybook/features/event/presentation/eventDetail/EventDetailFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
+import edu.iesam.diarybook.R
 import edu.iesam.diarybook.databinding.FragmentEventDetailBinding
 import edu.iesam.diarybook.features.event.domain.Event
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -38,6 +40,7 @@ class EventDetailFragment : Fragment() {
     private fun setUpObserver() {
         val observer = Observer<EventDetailViewModel.UiState> { uiState ->
             if (uiState.event != null) {
+                toolbarEdit(uiState.event)
                 bindData(uiState.event)
             }
         }
@@ -51,6 +54,24 @@ class EventDetailFragment : Fragment() {
             hour.text = event.hour
             duration.text = event.duration
             description.text = event.description
+        }
+    }
+
+    private fun toolbarEdit(event: Event) {
+        binding.toolbar.topAppBar.apply {
+            setBackgroundColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.md_theme_secondaryFixedDim_highContrast
+                )
+            )
+            setTitleTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.md_theme_onSecondaryFixedVariant_highContrast
+                )
+            )
+            title = event.title
         }
     }
 }
