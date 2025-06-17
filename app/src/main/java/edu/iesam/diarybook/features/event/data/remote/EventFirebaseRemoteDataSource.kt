74962391d41de3a1a667ebc722854c28f19d7ac0
Diego González Díaz <diego2612004@gmail.com>
2025-06-17 14:14:00 +0200

    Comentar tokens.

--- app/src/main/java/edu/iesam/diarybook/features/event/data/remote/EventFirebaseRemoteDataSource.kt
+++ app/src/main/java/edu/iesam/diarybook/features/event/data/remote/EventFirebaseRemoteDataSource.kt
@@ -1,19 +1,11 @@
 package edu.iesam.diarybook.features.event.data.remote
 
-import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.QuerySnapshot
 import edu.iesam.diarybook.features.event.domain.Event
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
-import okhttp3.FormBody
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
-import org.json.JSONObject
 import org.koin.core.annotation.Single
 import java.util.concurrent.TimeUnit
 
@@ -27,8 +19,8 @@ class EventFirebaseRemoteDataSource(
     private val client = OkHttpClient.Builder()
         .readTimeout(120, TimeUnit.SECONDS)
         .build()
-    private val token = "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"
-    private val apiKey = "5ff5aeaa84c30dad03669b307bf95125"
+    //private val token = "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"
+    //private val apiKey = "5ff5aeaa84c30dad03669b307bf95125"
 
     suspend fun getEventList(): List<Event> {
         val events = firestore.collection("events")
@@ -43,7 +35,7 @@ class EventFirebaseRemoteDataSource(
     }
 
     suspend fun createEvent(event: Event) {
-        val requestBody = JSONObject()
+        /*val requestBody = JSONObject()
             .put("inputs", event.title)
             .toString()
 
@@ -99,7 +91,7 @@ class EventFirebaseRemoteDataSource(
             }
         }
 
-        /*val json = JSONObject(responseUpload.body?.string() ?: "")
+        val json = JSONObject(responseUpload.body?.string() ?: "")
         Log.d("@dev", "Json: $json")
         val imageUrl = json.getJSONObject("data").getString("url")
         event.image = imageUrl*/
